a4ba8667070e74bea4a282ef26cff5ca78093fa9
susu <28025920@qq.com>
2016-04-19 20:39:42 +0800

    Using Masonry to implement AutoLayout.

--- AutoLayoutMasonry/AutoLayoutMasonry/ViewController.m
+++ AutoLayoutMasonry/AutoLayoutMasonry/ViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#define MAS_SHORTHAND_GLOBALS
+#define MAS_SHORTHAND
+#import "Masonry.h"
 @interface ViewController ()
 
 @end
@@ -16,12 +18,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self test2];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  两个视图的约束
+ */
+-(void)test2{
+    UIView* blueView=[[UIView alloc]init];
+    blueView.backgroundColor=[UIColor blueColor];
+    [self.view addSubview:blueView];
+    
+    UIView* redView=[[UIView alloc]init];
+    redView.backgroundColor=[UIColor redColor];
+    [self.view addSubview:redView];
+    
+    [blueView makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view);
+        make.right.equalTo(self.view);
+        make.top.equalTo(self.view).offset(20);
+        make.height.equalTo(40);
+    }];
+    
+    [redView makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(blueView.mas_centerX);
+        make.right.equalTo(blueView);
+        make.top.equalTo(blueView.bottom).multipliedBy(1.0).offset(30);
+        make.height.equalTo(40);
+    }];
+}
+
+/**
+ *  一个视图的约束
+ */
+-(void)test1{
+    UIView* blueView=[[UIView alloc]init];
+    blueView.backgroundColor=[UIColor blueColor];
+    [self.view addSubview:blueView];
+    
+    [blueView makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view);
+        make.right.equalTo(self.view);
+        make.top.equalTo(self.view);
+        make.height.equalTo(40);
+    }];
 }
 
 @end
